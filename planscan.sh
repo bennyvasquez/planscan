11073fa480d5f6a0ac18583fd35dd04b8e9a0931
bennyvasquez <benny@encylia.com>
2015-09-09 09:45:45 -0400

    excluding system user
    we don't want to alert for the system user's package ever.

--- planscan.sh
+++ planscan.sh
@@ -5,7 +5,7 @@ tmpfile="/tmp/planscan.txt"
 host=`hostname`
 resellerlist=`cut -d':' -f1 /var/cpanel/resellers`
 
-grep -H PLAN /var/cpanel/users/* | egrep -iv "DevClass|All\ The\|Farmstead|Garden\ Patch|Rancher" |egrep -v "$resellerlist" >> $tmpfile
+grep -H PLAN /var/cpanel/users/* | egrep -iv "DevClass|All\ The\|Farmstead|Garden\ Patch|Rancher|system" |egrep -v "$resellerlist" >> $tmpfile
 
 if [ -s $tmpfile ]; 
 	then
